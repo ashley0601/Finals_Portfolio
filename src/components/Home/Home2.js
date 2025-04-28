8622fd1e7d63b955174c3f804aea17af2d49abfc
Ashley Enallo <enalloashie@gmail.com>
2025-04-28 11:38:16 +0800

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,27 +19,25 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I’m a student passionate about building interactive web apps and experimenting with modern development tools.
               <br />
-              <br />I am fluent in classics like
+              <br />I enjoy programming, still learning skills in
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Python, JavaScript, HTML, CSS, and React.. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+             My field of Interest's are building new
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="purple">eb Technologies and Products </b> and
+                and also in areas related to {" "}
                 <b className="purple">
                   Blockchain.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              I’m excited to create meaningful products through code and continue exploring with <b className="purple">Node.js</b> and
               <i>
                 <b className="purple">
                   {" "}
